8b67dce481ceb0ea6399e879a1a909f99b81578e
Anon <>
2021-09-12 02:11:31 +0800

    fix: New config

--- utils/apollo/client.ts
+++ utils/apollo/client.ts
@@ -4,7 +4,8 @@ import fetch from "cross-fetch";
 export const client = new ApolloClient({
   link: new HttpLink({
     fetch,
-    uri: "https://api.bscgraph.org/subgraphs/name/pancakeswap/exchange-lite",
+    // uri: "https://api.bscgraph.org/subgraphs/name/pancakeswap/exchange-lite",
+    uri: "https://api.thegraph.com/subgraphs/name/autoshark-finance/exchange",
   }),
   cache: new InMemoryCache(),
 });
