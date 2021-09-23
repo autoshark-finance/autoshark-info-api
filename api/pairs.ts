c44fd4cc00f9f813ab4e5f38828124f9a0f93d02
Anon <crypto.eugene.ko@gmail.com>
2021-09-23 09:49:00 +0800

    fix: align to CMC endpoint

--- api/pairs.ts
+++ api/pairs.ts
@@ -5,6 +5,8 @@ import { return200, return500 } from "../utils/response";
 
 interface ReturnShape {
   [tokenIds: string]: {
+    base_id: string; // was added
+    quote_id: string; // was added
     pair_address: string;
     base_name: string;
     base_symbol: string;
@@ -12,7 +14,7 @@ interface ReturnShape {
     quote_name: string;
     quote_symbol: string;
     quote_address: string;
-    price: string;
+    last_price: string; // was modified from `price`
     base_volume: string;
     quote_volume: string;
     liquidity: string;
@@ -30,14 +32,16 @@ export default async function (req: VercelRequest, res: VercelResponse): Promise
       const t1Id = getAddress(pair.token1.id);
 
       accumulator[`${t0Id}_${t1Id}`] = {
+        base_id: t0Id,
         pair_address: pId,
         base_name: pair.token0.name,
         base_symbol: pair.token0.symbol,
         base_address: t0Id,
+        quote_id: t1Id,
         quote_name: pair.token1.name,
         quote_symbol: pair.token1.symbol,
         quote_address: t1Id,
-        price: pair.price,
+        last_price: pair.price,
         base_volume: pair.previous24hVolumeToken0,
         quote_volume: pair.previous24hVolumeToken1,
         liquidity: pair.reserveUSD,
